80b384d371ce5cd81f58b34481e94c46c0537e1c
DaeHyung <eogudlee@worksmobile.com>
2020-07-17 00:01:33 +0900

    Core Data fetch, add, delete 기능 구현

--- CoreDataTableView/ListTableViewController.swift
+++ CoreDataTableView/ListTableViewController.swift
@@ -12,87 +12,143 @@ import CoreData
 
 class ListTableViewController: UITableViewController {
 
+    
+    
+    //MARK: Properties
+    lazy var list: [NSManagedObject] = {
+        return self.fetch()
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        //네비게이션 오른쪽 위에 추가 버튼 구현
+        let addBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add(_:)))
+        self.navigationItem.rightBarButtonItem = addBtn
+        
+        
+        
     }
 
+    
+    @objc func add(_ sender: Any){
+        let alert = UIAlertController(title: "게시글 등록", message: nil, preferredStyle: .alert)
+        alert.addTextField(configurationHandler: {$0.placeholder = "제목"})
+        alert.addTextField(configurationHandler: {$0.placeholder = "내용"})
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default){(_) in
+            guard let title = alert.textFields?.first?.text, let contents = alert.textFields?.last?.text else{
+                return
+            }
+            
+            //값을 저장, 성공이면 테이블 리로드
+            if self.save(title: title, contents: contents) == true {
+                self.tableView.reloadData()
+            }
+        })
+        self.present(alert, animated: false)
+    }
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.list.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        //데이터 가져오기
+        let record = self.list[indexPath.row]
+        let title = record.value(forKey: "title") as? String
+        let contents = record.value(forKey: "contents") as? String
+        
+        //셀을 생성하고 대입
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = contents
+        
         // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        let object = self.list[indexPath.row]
+        
+        if self.delete(object: object){
+            //코어 데이터에서 삭제되면 배열 목록과 테이블 뷰의 행도 삭제
+            self.list.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     
     //MARK: Core Data
-    func fetch() -> [NSManageeObject]{
+    
+    
+    func fetch() -> [NSManagedObject]{
         //앱 델리게이트 객체 참조
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        //관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext
+        //요청 객체 생성
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Board")
+        //데이터 가져오기
+        let result = try! context.fetch(fetchRequest)
+        
+        return result
     }
+    
+    func save(title: String, contents: String) -> Bool{
+        //앱 델리게이트 객체 참조
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        //관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext
+        //관리 객체 생성, 값 설정
+        let object = NSEntityDescription.insertNewObject(forEntityName: "Board", into: context)
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        //영구 저장소에 커밋되고 나면 list 프로퍼티에 추가
+        do{
+            try context.save()
+            self.list.append(object)
+            return true
+        }catch{
+            context.rollback()
+            return false
+        }
+    }
+    
+    
+    func delete(object: NSManagedObject) -> Bool{
+        //앱 델리게이트 객체 참조
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        //관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext
+        //컨텍스트로부터 해당 객체 삭제
+        context.delete(object)
+        
+        //영구 저장소에 커밋
+        do{
+            try context.save()
+            return true
+        }catch{
+            context.rollback()
+            return false
+        }
+    }
+    
 }
